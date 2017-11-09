681e9bbc064683ea1954a3a27f9a9fa79d3bbcad
Caratpine <bepox0531@gmail.com>
2017-11-09 20:09:43 +0800

    .

--- fopnp/chapter07/client.py
+++ fopnp/chapter07/client.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-import argparse
+import zen_utils
 import random
 import socket
-import zen_utils
+import argparse
 
 
 def client(address, cause_error=False):
@@ -20,11 +20,11 @@ def client(address, cause_error=False):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Example client")
-    parser.add_argument('host', help="IP or hostname")
+    parser = argparse.ArgumentParser(description='Example client')
+    parser.add_argument('host', help='IP or hostname')
+    parser.add_argument('-e', action='store_true', help='cause an error')
     parser.add_argument('-p', metavar='port', type=int, default=1060,
                         help='TCP port (default 1060)')
-    parser.add_argument('-e', action="store_true", help="cause an error")
     args = parser.parse_args()
     address = (args.host, args.p)
     client(address, args.e)
