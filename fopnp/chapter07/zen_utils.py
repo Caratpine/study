dc7b8779cd324291f79e8fc2f8e9fc0839af3e2f
Caratpine <bepox0531@gmail.com>
2017-11-09 20:03:24 +0800

    .

--- fopnp/chapter07/zen_utils.py
+++ fopnp/chapter07/zen_utils.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env/ python3
 
-import argparse
-import socket
 import time
+import socket
+import argparse
 
-aphorisms = {
-    b'Beautiful is better than?': b'Ugly.',
-    b'Explicit is better than?': b'Implicit.',
-    b'Simple is better than?': b'Complex.',
-}
+aphorisms = {b'Beautiful is better than?': b'Ugly.',
+             b'Explicit is better than?': b'Implicit.',
+             b'Simple is better than?': b'Complex.'}
 
 
 def get_answer(aphorism):
     time.sleep(0.0)
-    return aphorisms.get(aphorism, b'Error: unknow aphorism')
+    return aphorisms.get(aphorism, b'Error: unknown aphorism.')
 
 
 def parse_command_line(description):
+    """Parse command line and return a socket address."""
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument('host', help="IP or hostname")
+    parser.add_argument('host', help='IP or hostname')
     parser.add_argument('-p', metavar='port', type=int, default=1060,
                         help='TCP port (default 1060)')
     args = parser.parse_args()
@@ -27,19 +26,19 @@ def parse_command_line(description):
 
 
 def create_srv_socket(address):
-    listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    listener.bind(address)
-    listener.listen(64)
-    print("Listening at {}".format(address))
-    return listener
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(address)
+    sock.listen(64)
+    print("Listening at: {}".format(address))
+    return sock
 
 
-def accept_connections_forever(listener):
+def accept_connections_forever(sock):
     while True:
-        sock, address = listener.accept()
-        print("Accepted connection from {}".format(address))
-        handle_conversation(sock, address)
+        sc, address = sock.accept()
+        print("Accept connection from {}".format(address))
+        handle_conversation(sc, address)
 
 
 def handle_conversation(sock, address):
@@ -63,10 +62,10 @@ def handle_request(sock):
 def recv_until(sock, suffix):
     message = sock.recv(4096)
     if not message:
-        raise EOFError("socket closed")
+        raise EOFError('socket closed')
     while not message.endswith(suffix):
         data = sock.recv(4096)
         if not data:
-            raise EOFError("received {!r} then socket closed".format(message))
+            raise IOError('received {!r} then socket closed'.format(message))
         message += data
     return message
