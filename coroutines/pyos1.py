6e3f6d73e8e77d6fbeb52c39e930726c0722ecdc
Caratpine <bepox0531@gmail.com>
2017-12-19 20:33:16 +0800

    .

--- coroutines/pyos1.py
+++ coroutines/pyos1.py
@@ -61,20 +61,44 @@ class Scheduler(object):
             self.schedule(task)
 
 
+class NewTask(SystemCall):
+    def __init__(self, target):
+        self.target = target
+
+    def handle(self):
+        tid = self.sched.new(self.target)
+        self.task.sendval = tid
+        self.sched.schedule(self.task)
+
+
+class KillTask(SystemCall):
+    def __init__(self, tid):
+        self.tid = tid
+
+    def handle(self):
+        task = self.sched.taskmap.get(self.tid, None)
+        if task:
+            task.target.close()
+            self.task.sendval = True
+        else:
+            self.task.sendval = False
+        self.sched.schedule(self.task)
+
+
 if __name__ == '__main__':
     def foo():
         mytid = yield GetTid()
-        for i in range(10):
+        while True:
             print("I'm foo", mytid)
             yield
 
-    def bar():
-        mytid = yield GetTid()
-        for i in range(10):
-            print("I'm bar", mytid)
+    def main():
+        child = yield NewTask(foo())
+        for i in range(5):
             yield
+        yield KillTask(child)
+        print("main done")
 
     sched = Scheduler()
-    sched.new(foo())
-    sched.new(bar())
+    sched.new(main())
     sched.mainloop()
