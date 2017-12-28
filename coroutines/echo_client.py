36b752f7e17ff6eb2216a41a503eca84d72a5a9e
Caratpine <bepox0531@gmail.com>
2017-12-28 21:20:26 +0800

    .

--- coroutines/echo_client.py
+++ coroutines/echo_client.py
@@ -24,6 +24,7 @@ class EchoClientProtocol(asyncio.Protocol):
 loop = asyncio.get_event_loop()
 message = "Hello world!"
 coro = loop.create_connection(lambda: EchoClientProtocol(message, loop), '127.0.0.1', 8888)
-loop.run_until_complete(coro)
+print(coro)
+transport, protocol = loop.run_until_complete(coro)
 loop.run_forever()
 loop.close()
