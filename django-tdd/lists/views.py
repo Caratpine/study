742137fb1528d187f3fe291f6d30192d06e52bb2
Caratpine <bepox0531@gmail.com>
2018-06-20 22:27:39 +0800

    .

--- django-tdd/lists/views.py
+++ django-tdd/lists/views.py
@@ -8,7 +8,12 @@ def home_page(request):
     if request.method == 'POST':
         new_item_text = request.POST['item_text']
         Item.objects.create(text=new_item_text)
-        return redirect('/')
+        return redirect('/test')
 
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
+
+
+def view_list(request):
     items = Item.objects.all()
     return render(request, 'home.html', {'items': items})
