4a22db7e6919bc54923d650cd6947c94cb20cdb5
Caratpine <bepox0531@gmail.com>
2018-07-03 20:45:24 +0800

    .

---
+++ test_mock.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+
+from mock import MagicMock
+
+
+class ProductionClass:
+    def method(self):
+        self.something(1, 2, 3)
+    
+    def something(self, a, b, c):
+        pass
+
+real = ProductionClass()
+real.something = MagicMock()
+real.method()
+real.something.assert_called_once_with(1, 2, 3)
