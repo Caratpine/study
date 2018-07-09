f9f4268ef198d3135a0b668802979dab21a40747
Caratpine <bepox0531@gmail.com>
2018-07-09 22:41:08 +0800

    .

--- test_mock.py
+++ test_mock.py
@@ -1,8 +1,8 @@
 # coding=utf-8
 
 from mock import MagicMock, patch
-import test_client
-# from test_client import Foo
+# import test_client
+from .test_client import Foo
 
 
 class ProductionClass:
@@ -19,13 +19,23 @@ real.closer(mock)
 mock.close.assert_called_with()
 
 
-def some_function():
-    instance = test_client.Foo()
-    return instance.method()
+# def some_function():
+#     instance = test_client.Foo()
+#     return instance.method()
 
 
-with patch('test_client.Foo') as mock:
-    instance = mock.return_value
-    instance.method.return_value = 'fuck'
-    result = some_function()
-    assert result == 'fuck'
+# with patch('test_client.Foo') as mock:
+#     instance = mock.return_value
+#     instance.method.return_value = 'fuck'
+#     result = some_function()
+#     assert result == 'fuck'
+
+
+original = Foo.method
+mock = MagicMock()
+
+
+@patch.object(Foo, 'method', mock.method)
+def test():
+    assert Foo.method() == mock.method.return_value
+    assert 1 == 2
