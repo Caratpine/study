bb6f5124702ff88873232af8e246e5efc95978a7
xdd1874 <bepox0531@gmail.com>
2018-11-13 10:46:45 +0800

    .

--- gevent/test_queue.py
+++ gevent/test_queue.py
@@ -3,25 +3,53 @@
 import gevent
 from gevent.queue import Queue
 
-q = Queue()
+# q = Queue()
+#
+#
+# def consumer(name):
+#     while not q.empty():
+#         print('%s got product %s' %(name, q.get()))
+#         gevent.sleep(0)
+#
+#     print('%s Quit' % name)
+#
+#
+# def producer():
+#     for i in range(1, 10):
+#         q.put(i)
+#
+#
+# gevent.joinall([
+#     gevent.spawn(producer),
+#     gevent.spawn(consumer, 'xpb'),
+#     gevent.spawn(consumer, 'xdd'),
+#     gevent.spawn(consumer, 'fuck')
+# ])
 
 
-def consumer(name):
-    while not q.empty():
-        print('%s got product %s' %(name, q.get()))
+tasks = Queue()
+
+
+def worker(n):
+    while not tasks.empty():
+        task = tasks.get()
+        print('Worker %s got task %s' % (n, task))
         gevent.sleep(0)
 
-    print('%s Quit' % name)
+    print('Quit time!')
 
 
-def producer():
-    for i in range(1, 10):
-        q.put(i)
+def boss():
+    for i in range(1, 25):
+        tasks.put_nowait(i)
 
 
 gevent.joinall([
-    gevent.spawn(producer),
-    gevent.spawn(consumer, 'xpb'),
-    gevent.spawn(consumer, 'xdd'),
-    gevent.spawn(consumer, 'fuck')
+    gevent.spawn(boss),
+    gevent.spawn(worker, 'xpb'),
+    gevent.spawn(worker, 'xdd'),
+    gevent.spawn(worker, 'fuck')
 ])
+
+
+
