1f65a9bb32dd3a71147f579caa32ec57ec4ee4a4
xdd1874 <bepox0531@gmail.com>
2018-11-16 14:20:28 +0800

    .

--- huey_study/tasks.py
+++ huey_study/tasks.py
@@ -6,6 +6,10 @@ from config import h
 @h.task()
 def count_beans(num):
     print(f'-- counted {num} beans --')
+    return f'Counted {num} beans'
 
 
-
+@h.task(retries=3)
+def try_thrice():
+    print('try...')
+    raise Exception('nope')
