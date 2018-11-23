e8f38e8f268958cc1a04065f6f8e2149ad0c9132
xdd1874 <bepox0531@gmail.com>
2018-11-23 10:32:40 +0800

    .

--- socket_io/app.py
+++ socket_io/app.py
@@ -1,8 +1,7 @@
 # coding=utf-8
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_socketio import SocketIO
-
 
 
 app = Flask(__name__)
@@ -11,6 +10,16 @@ app.config['SECRET_KEY'] = 'hello world'
 socket_io = SocketIO()
 
 socket_io.init_app(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@socket_io.on('message')
+def handle_message(message):
+    print('received message: '+ message)
 
 
 if __name__ == '__main__':
