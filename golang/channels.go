f1cc5815502353a2ed7cbff8fb53808492a1321d
xdd1874 <bepox0531@gmail.com>
2019-07-29 13:34:59 +0800

    .

---
+++ golang/channels.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum
+}
+
+
+func main() {
+	ch := make(chan int, 2)
+	ch <- 1
+	ch <- 2
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+}
