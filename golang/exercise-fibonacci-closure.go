61aff2dffb5c579fbef7b355992d82e31906b10e
xdd1874 <bepox0531@gmail.com>
2019-07-30 20:42:14 +0800

    .

---
+++ golang/exercise-fibonacci-closure.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func fibonacci3() func() int {
+	x, y , z := 0, 1, 0
+	return func() int {
+		z, x, y = x, y, x+y
+		return z
+	}
+}
+
+func main() {
+	f := fibonacci3()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}
