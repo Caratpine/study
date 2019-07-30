c274a0d05a1381704dd730f11df02faa5cc476c1
xdd1874 <bepox0531@gmail.com>
2019-07-30 20:11:12 +0800

    .

--- golang/exercise-maps.go
+++ golang/exercise-maps.go
@@ -1,11 +1,18 @@
 package main
 
+import "strings"
+import "golang.org/x/tour/wc"
+
 func WordCount(s string) map[string]int {
-	var dict = map[string]int{}
-	for _, v := range s {
-		dict[string(v)]++
+	m := make(map[string]int)
+	for _, v := range strings.Fields(s) {
+		m[v]++
 	}
-	return dict
+	return m
+}
+
+func main() {
+	wc.Test(WordCount)
 }
 
 
