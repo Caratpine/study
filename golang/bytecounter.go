002288a899a475198293f14529d1f51cc79fc60c
xdd1874 <bepox0531@gmail.com>
2019-10-18 15:22:20 +0800

    .

---
+++ golang/bytecounter.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+type ByteCounter int
+
+func (c *ByteCounter) Write(p []byte) (int, error) {
+	*c += ByteCounter(len(p))
+	return len(p), nil
+}
+
+func main() {
+	var c ByteCounter
+	_, err := c.Write([]byte("hello"))
+	if err != nil {
+		return
+	}
+	fmt.Println(c)
+	c = 0
+	var name = "Dolly"
+	fmt.Fprintf(&c, "hello, %s", name)
+	fmt.Println(c)
+
+}
