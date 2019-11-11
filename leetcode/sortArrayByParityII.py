69788005ac8188feec055b0bc98f1ad02f532764
xdd1874 <bepox0531@gmail.com>
2019-11-11 19:48:01 +0800

    922. sort-array-by-parity-ii

--- leetcode/sortArrayByParityII.py
+++ leetcode/sortArrayByParityII.py
@@ -3,14 +3,11 @@ from typing import List
 
 class Solution:
     def sortArrayByParityII(self, A: List[int]) -> List[int]:
-        left = 0
-        right = len(A) - 1
-        while left < right:
-            while left % 2 !=  A[left] % 2 and left < len(A):
-                left += 1
-            while right % 2 != A[right] % 2 and right >= 0:
-                right -= 1
-
-            if left < right:
-                A[left], A[right] = A[right], A[left]
+        j = 1
+        for i in range(0, len(A), 2):
+            if A[i] % 2:
+                while A[j] % 2:
+                    j += 2
+                A[i], A[j] = A[j], A[i]
         return A
+
