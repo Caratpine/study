1df39332a0ba27e43c060c9014fd511926b1ee82
xdd1874 <bepox0531@gmail.com>
2020-02-04 22:18:01 +0800

    409. longest-palindrome

---
+++ leetcode/longestPalindrome1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        d = {}
+        for v in s:
+            if v in d.keys():
+                d[v] += 1
+            else:
+                d[v] = 1
+
+        sum = 0
+        for k, v in d.items():
+            sum += v // 2 * 2
+            if sum % 2 == 0 and v % 2 == 1:
+                sum += 1
+        return sum
+    
