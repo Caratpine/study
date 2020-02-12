90732215dab22dbf0607464381c743ea9299d2a5
xdd1874 <bepox0531@gmail.com>
2020-02-12 18:06:30 +0800

    lcof 54. er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof

---
+++ leetcode/lcof/kthLargest.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def kthLargest(self, root: TreeNode, k: int) -> int:
+        ans = []
+
+        def dfs(root):
+            if not root:
+                return
+            dfs(root.left)
+            ans.append(root.val)
+            dfs(root.right)
+
+        dfs(root)
+        return ans[-k]
