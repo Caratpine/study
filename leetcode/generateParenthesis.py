3a870631eb6bb6a12784bfe42dfeb61034831b6d
xdd1874 <bepox0531@gmail.com>
2020-02-19 20:34:02 +0800

    22. generate-parentheses

--- leetcode/generateParenthesis.py
+++ leetcode/generateParenthesis.py
@@ -31,6 +31,22 @@ class Solution:
         generate()
         return ans
 
+    def generateParenthesis1(self, n: int) -> List[str]:
+        ans = []
+
+        def backtracing(s, n, left=0, right=0):
+            if len(s) == 2 * n:
+                ans.append(s)
+                return
+            else:
+                if left < n:
+                    backtracing(s + '(', n, left + 1, right)
+                if right < left:
+                    backtracing(s + ')', n, left, right + 1)
+
+        backtracing('', n)
+        return ans
+
 
 if __name__ == '__main__':
     Solution().generateParenthesis(2)
