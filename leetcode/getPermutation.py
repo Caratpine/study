5d6c43a6fbc466f3e9814433753c9a9c56a42466
xdd1874 <bepox0531@gmail.com>
2020-02-20 21:54:47 +0800

    60. permutation-sequence

--- leetcode/getPermutation.py
+++ leetcode/getPermutation.py
@@ -3,18 +3,25 @@
 
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
-        nums = list(range(1, n + 1))
-        results = []
+        factorial = [1 for _ in range(n + 1)]
+        for i in range(2, n + 1):
+            factorial[i] = factorial[i - 1] * i
+        used = [False for _ in range(n + 1)]
+        path = []
 
-        def backtracking(tmp):
-            if len(nums) == len(tmp):
-                results.append(tmp[:])
-            else:
-                for n in nums:
-                    if n in tmp:
-                        continue
-                    tmp.append(n)
-                    backtracking(tmp)
-                    tmp.pop()
-        backtracking([])
-        return ''.join(map(str, results[k - 1]))
+        def dfs(used, n, index, path, k):
+            if index == n:
+                return
+            c = factorial[n - index - 1]
+            for i in range(1, n + 1):
+                if used[i]:
+                    continue
+                if c < k:
+                    k -= c
+                    continue
+                path.append(i)
+                used[i] = True
+                dfs(used, n, index + 1, path, k)
+
+        dfs(used, n, 0, path, k)
+        return ''.join([str(i) for i in path])
